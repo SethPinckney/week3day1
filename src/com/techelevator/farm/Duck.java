c78955637f6edce60074fb0dba16d724dbe1aaf2
sethpinckney <sethpinckney@10.0.1.16>
2015-10-12 11:27:49 -0400

    made Duck class

---
+++ src/com/techelevator/farm/Duck.java
@@ -0,0 +1,13 @@
+package com.techelevator.farm;
+
+public class Duck implements Animal {
+
+	public String getName() {
+		return "Duck";
+	}
+
+	public String getSound() {
+		return "Quack!";
+	}
+
+}
